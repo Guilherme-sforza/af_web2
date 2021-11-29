47a3a38e206fee653aa2b4b33d8fb6ecee821e83
Guilherme-sforza <70664346+Guilherme-sforza@users.noreply.github.com>
2021-11-29 00:10:27 -0300

    Add files via upload

---
+++ DesenWeb-master/src/components/MyCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    {{MyCard}}
+    <v-img
+      src="../assets/ghost_type.png"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      {{cardTitle}}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{cardSubtitle}}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="purple lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        {{cardDescription}}
+      </div>
+    </v-expand-transition>
+  </v-card>
+  
+</template>
+
+<script>
+  export default {
+    props:['cardid', 'cardTitle', 'cardDescription', 'cardSubtitle'],
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
---
+++ DesenWeb-master/src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        cards:[
+            {title: "Ghost Type", subtitle: "Kanto", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", image:"hp1.jpg", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Johto", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Hoenn", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Sinnoh", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Unova", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Kalos", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Alola", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+            {title: "Ghost Type", subtitle: "Galar", author:"J.K.Rowling", date:"07/26/1997", pages:"223", genre:"Romance/Fantasia", note:"4.7", description:"Eight regions have appeared in the core series of Pokémon games. They are, in order of appearance, Kanto, Johto, Hoenn, Sinnoh (formerly known as Hisui), Unova, Kalos, Alola, and Galar. Each region has been the setting of at least two games in the generation in which it debuts."},  
+        ],
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+  })
+  
